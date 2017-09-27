36398bef103b3acfc08168dc2ca001aafb358e6b
Felipe Cardoso <felipe.cardoso@serpro.gov.br>
2017-09-27 19:25:42 -0300

    integração gulp + nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,76 @@
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
+"use strict";
 
-// pull in the project TypeScript config
-const tsProject = ts.createProject('tsconfig.json');
+//******************************************************************************
+//* DEPENDENCIES
+//******************************************************************************
 
-gulp.task('scripts', () => {
-  const tsResult = tsProject.src()
-  .pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest('dist'));
+var gulp        = require("gulp"),
+    tslint      = require("gulp-tslint"),
+    tsc         = require("gulp-typescript"),
+    runSequence = require("run-sequence"),
+    nodemon     = require('gulp-nodemon');
+
+//******************************************************************************
+//* LINT
+//******************************************************************************
+gulp.task("lint", function() {
+    var config =  { formatter: "verbose" };
+    return gulp.src([
+        "src/**/**.ts"
+    ])
+    .pipe(tslint(config))
+    .pipe(tslint.report());
 });
 
-gulp.task('watch', ['scripts'], () => {
-  gulp.watch('src/**/*.ts', ['scripts']);
+//******************************************************************************
+//* BUILD
+//******************************************************************************
+var tstProject = tsc.createProject("tsconfig.json", { typescript: require("typescript") });
+
+gulp.task("build", function() {
+    return gulp.src([
+        "src/**/*.ts"
+    ])
+    .pipe(tstProject())
+    .on("error", function (err) {
+        process.exit(1);
+    })
+    .js.pipe(gulp.dest("dist/"));
 });
 
-gulp.task('default', ['watch']);
+//******************************************************************************
+//* WATCH
+//******************************************************************************
+gulp.task('watch', ['default'], () => {
+    gulp.watch('src/**/*.ts', ['default']);
+});
+
+//******************************************************************************
+//* RUN
+//******************************************************************************
+gulp.task('run', ['lint', 'build'], () => {
+    var stream = nodemon({ 
+        script: 'dist/index.js', 
+        ext: 'ts', 
+        ignore: [
+            'node_modules/'         
+        ],
+        tasks: ['lint', 'build']
+    })
+
+    stream
+    .on('restart', function () {
+        console.log('Restarted!')
+    })
+    .on('crash', function() {
+        console.error('Application has crashed!\n')
+        stream.emit('restart', 10)  // restart the server in 10 seconds 
+    })
+});
+
+//******************************************************************************
+//* DEFAULT
+//******************************************************************************
+gulp.task("default", function (cb) {
+  runSequence("lint", "build", cb);
+});
