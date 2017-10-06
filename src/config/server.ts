f5c2b77dbb78f317e1cfc6be5377ce56c57d9c29
Felipe Cardoso <felipe.cardoso@serpro.gov.br>
2017-10-06 20:08:58 -0300

    Tipando as entradas do construtor da classe Server

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,11 +1,13 @@
 import { injectable, inject } from 'inversify';
 import TYPES from '../constant/types';
 import * as restify from 'restify';
+import * as bunyan from 'bunyan';
+import { InversifyRestifyServer } from 'inversify-restify-utils';
 
 @injectable()
 export class Server {
 
-    constructor(@inject(TYPES.ServerRestify) server, @inject(TYPES.Log) logger) {
+    constructor(@inject(TYPES.ServerRestify) server: InversifyRestifyServer, @inject(TYPES.Log) logger: bunyan) {
         let port = process.env.PORT || 3000;
         port = (typeof port === 'string') ? parseInt(port, 10) : port;
         server
