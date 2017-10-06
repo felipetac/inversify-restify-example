efc9265471bdbcf9147b2c3206dd51c750acda8a
Felipe Toscano <felipe.toscano@gmail.com>
2017-10-05 21:37:36 -0300

    Remoção das libs não mais utilizadas

--- src/config/log.ts
+++ src/config/log.ts
@@ -1,21 +1,3 @@
-/*import * as morgan from 'morgan';
-import fs = require('fs');
-import path = require('path');
-import rfs = require('rotating-file-stream');
-
-let logDirectory = path.join(__dirname, 'log');
-
-// Verifica se o diretório de log existe
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-
-// Create a rotating write stream
-let accessLogStream = rfs('access.log', {
-    interval: '1d', // Estabelece logs diários d-1
-    path: logDirectory
-});
-
-const logger = morgan('combined', {stream: accessLogStream});*/
-
 import * as restify from 'restify';
 import fs = require('fs');
 import path = require('path');
@@ -24,8 +6,6 @@ import * as bunyan from 'bunyan';
 const logDirectory = path.join(__dirname, '/../log');
 
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-
-console.log(logDirectory);
 
 const logger = bunyan.createLogger({
     name: 'helloapi',
